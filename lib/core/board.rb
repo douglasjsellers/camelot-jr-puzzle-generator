9aba434fbb961ca0f5b1044417b889f59b845c8d
Douglas Sellers <douglas.sellers@gmail.com>
2017-01-27 10:55:41 -0800

    Made it so that now you can add a basic piece vertical piece

--- lib/core/board.rb
+++ lib/core/board.rb
@@ -4,10 +4,19 @@ class Board
                        [0,0,0,0,0,0],
                        [0,0,0,0,0,0],
                        [0,0,0,0,0,0]]
+    @pieces = []
+  end
+
+  def has_piece_at?( x, y )
+    @raw_board_array[y][x] == 1
   end
   
-  def place_piece( piece, position, location )
-    
+  def place_piece( piece, position, x, y )
+    if( position.height > position.width )
+      place_vertical_piece( piece, x, y )
+    else
+      place_horizontal_piece( piece, x, y )
+    end
   end
 
   def valid_board?
@@ -24,6 +33,18 @@ class Board
     end
     
     string_to_return
+  end
+  
+  private
+  def place_vertical_piece( piece, x, y )
+    (0..(piece.size - 1 ) ).each do |adder|
+       @raw_board_array[y + adder ][x] = 1
+    end
+    @pieces << piece
+    return true
+  end
+
+  def place_horizontal_piece( piece, x, y )
   end
   
   
--- tests/specs/board_spec.rb
+++ tests/specs/board_spec.rb
@@ -1,6 +1,6 @@
 require_relative '../spec_helper'
 
-describe Board do
+describe Board do 
   it "should be able to run the constructor" do
     board = Board.new
     board.should_not == nil
@@ -9,8 +9,17 @@ describe Board do
   it "should nicely draw an empty board" do
     board = Board.new
     board.to_s.should_not == nil
-    puts board.to_s    
   end
 
+  it "should correctly place a vertical place a red piece" do
+    board = Board.new
+    red_piece = RedPiece.new
+    board.place_piece( red_piece, red_piece.positions.first, 0, 0 )
+    board.has_piece_at?( 0, 0 ).should == true
+    board.has_piece_at?( 0, 1 ).should == true
+    board.has_piece_at?( 0, 2 ).should == false
+    board.has_piece_at?( 1, 0 ).should == false
+  end
+  
 
 end
