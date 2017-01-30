565387c9ab0d49a4c02c6a898af2093eb6c0d084
Douglas Sellers <douglas.sellers@gmail.com>
2017-01-30 12:34:03 -0800

    Removed the rspec deprecated syntax

--- tests/specs/solution_spec.rb
+++ tests/specs/solution_spec.rb
@@ -7,15 +7,15 @@ describe Solution do
     knight = Knight.new
     princess = Princess.new
     
-    board.place_piece( orange_piece, orange_piece.positions.first, 5, 0 ).should == true
-    board.place_piece( princess, princess.positions.first, 0, 0 ).should == true
-    board.place_piece( knight, knight.positions.first, 5, 1 ).should == true
+    expect( board.place_piece( orange_piece, orange_piece.positions.first, 5, 0 ) ).to eq( true )
+    expect( board.place_piece( princess, princess.positions.first, 0, 0 ) ).to eq( true )
+    expect( board.place_piece( knight, knight.positions.first, 5, 1 ) ).to eq( true )
     
     solution = Solution.new( board, [BluePiece.new] )
 
-    solution.has_solution?.should == true
+    expect( solution.has_solution? ).to eq( true )
     final_board = solution.final_position_board
-    final_board.should_not == nil
+    expect ( final_board ).not_to eq( nil )
   end
   
 end
