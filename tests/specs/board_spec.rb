514ff48392b3aad43e16f399420ca5964d76359c
Douglas Sellers <douglas.sellers@gmail.com>
2017-02-08 11:04:02 -0800

    Made it so that you can disable the specs

--- tests/specs/board_spec.rb
+++ tests/specs/board_spec.rb
@@ -2,17 +2,19 @@ require_relative '../spec_helper'
 VERTICAL = true
 HORIZONTAL = false
 
-describe Board do 
+describe Board do
+  @disabled = false
+  
   it "should be able to run the constructor" do
     board = Board.new
     board.should_not == nil
-  end
+  end unless @disabled
 
   it "should nicely draw an empty board" do
     board = Board.new
     board.to_s.should_not == nil
     board.to_s.should == "000000\n000000\n000000\n000000\n"
-  end
+  end unless @disabled
 
   it "should correctly place a vertical place a red piece" do
     board = Board.new
@@ -22,7 +24,7 @@ describe Board do
     board.has_piece_at?( 0, 1 ).should == true
     board.has_piece_at?( 0, 2 ).should == false
     board.has_piece_at?( 1, 0 ).should == false
-  end
+  end unless @disabled
 
   it "should not be able to place one red piece on top of the other red piece" do
     board = Board.new
@@ -35,14 +37,16 @@ describe Board do
     board.place_piece( red_piece, red_piece.positions.first, 0, 0 ).should == false
     board.has_piece_at?( 0, 0 ).should == true
     board.has_piece_at?( 0, 1 ).should == true
-  end
+  end unless @disabled
+
   
   it "should nicely draw a board with vertical piece" do
     board = Board.new
     red_piece = RedPiece.new
     board.place_piece( red_piece, red_piece.positions.first, 0, 0 ).should == true
     board.to_s.should == "000000\n000000\n100000\n100000\n"
-  end
+  end unless @disabled
+
 
   it "should nicely draw a vertical piece with a stair on the top" do
     board = Board.new
@@ -50,7 +54,8 @@ describe Board do
     board.place_piece( blue_piece, Position.new( blue_piece, VERTICAL, [1, '/'] ), 0, 0  )
     board.to_s.should == "000000\n000000\n\/00000\n100000\n"
     
-  end
+  end unless @disabled
+
 
   it "should be able to add a horizontal piece" do
     board = Board.new
@@ -61,7 +66,8 @@ describe Board do
     
     board.to_s.should == "000000\n000000\n000000\n1\/0000\n"
     
-  end
+  end unless @disabled
+
 
   it "should not be able to double place a horizontal piece" do
     board = Board.new
@@ -73,7 +79,8 @@ describe Board do
     board.has_piece_at?( 1, 0 ).should == true
     
     board.to_s.should == "000000\n000000\n000000\n1\/0000\n"
-  end
+  end unless @disabled
+
 
   it "should not be able to overlay a horizontal piece on a vertical piece" do
     board = Board.new
@@ -86,7 +93,8 @@ describe Board do
     board.has_piece_at?( 0, 0 ).should == true
     board.has_piece_at?( 0, 1 ).should == true
     
-  end
+  end unless @disabled
+
 
   it "should not be able to place a piece with nothing below it" do
     board = Board.new
@@ -96,7 +104,8 @@ describe Board do
     board.has_piece_at?( 0, 1 ).should == false
     board.has_piece_at?( 1, 1 ).should == false
     
-  end
+  end unless @disabled
+
 
   it "should not be able to place a piece with nothing below it with stairs on left" do
     board = Board.new
@@ -106,7 +115,8 @@ describe Board do
     board.has_piece_at?( 0, 1 ).should == false
     board.has_piece_at?( 1, 1 ).should == false
     
-  end
+  end unless @disabled
+
   
   it "should not be able to place a three length piece with nothing below it" do
     board = Board.new
@@ -119,7 +129,8 @@ describe Board do
 
     board.place_piece( green_piece, Position.new( green_piece, HORIZONTAL, [1, 1, '/'] ), 0, 1  ).should == false
     
-  end
+  end unless @disabled
+
 
   it "should not be possible to place the stairs down" do
     board = Board.new
@@ -129,13 +140,17 @@ describe Board do
     board.has_piece_at?( 0, 0 ).should == false
     board.has_piece_at?( 0, 1 ).should == false
     
-  end
+  end unless @disabled
 
-  it "should not allow pieces to be extended off of either end" do
+
+  it "should not allow pieces to be ext  end unless @disabled
+ed off of either   end unless @disabled
+" do
     board = Board.new
     green_piece = GreenPiece.new
     board.place_piece( green_piece, Position.new( green_piece, HORIZONTAL, ['\\', 1, 1] ), 5, 0  ).should == false
-  end
+  end unless @disabled
+
 
   it "should be able to place a princess" do
     board = Board.new
@@ -143,7 +158,8 @@ describe Board do
     board.place_piece( princess, princess.positions.first, 0, 0 ).should == true
     board.to_s.should == "000000\n000000\n000000\nP00000\n"
     
-  end
+  end unless @disabled
+
 
   it "should be able to place a knight" do
     board = Board.new
@@ -151,7 +167,8 @@ describe Board do
     board.place_piece( knight, knight.positions.first, 0, 0 ).should == true
     board.to_s.should == "000000\n000000\n000000\nK00000\n"
   
-  end
+  end unless @disabled
+
 
   it "should be able to find the princess" do
     board = Board.new
@@ -163,7 +180,8 @@ describe Board do
     x, y = board.princess_location
     x.should == 0
     y.should == 2
-  end
+  end unless @disabled
+
 
   it "should be able to find the knight" do
     board = Board.new
@@ -175,7 +193,8 @@ describe Board do
     x, y = board.knight_location
     x.should == 5
     y.should == 2
-  end
+  end unless @disabled
+
 
   it "should be able to find both the princess ande the knight" do
     board = Board.new
@@ -196,7 +215,8 @@ describe Board do
     x.should == 0
     y.should == 2
     
-  end
+  end unless @disabled
+
 
   it "should be able to determine if there is a knight" do
     board = Board.new
@@ -212,7 +232,8 @@ describe Board do
     board.place_piece( knight, knight.positions.first, 5, 2 ).should == true
 
     board.has_knight?.should == true
-  end
+  end unless @disabled
+
 
   it "should be able to deterime if there is a princess" do
     board = Board.new
@@ -229,7 +250,8 @@ describe Board do
     board.place_piece( princess, princess.positions.first, 0, 0 ).should == true
     board.has_princess?.should == true
     
-  end
+  end unless @disabled
+
   
 
   it "should be possible to make a distinct clone" do
@@ -249,7 +271,8 @@ describe Board do
 
     expect( board.place_piece( princess, princess.positions.first, 0, 0 ) ).to eq( true )
     expect( cloned_board.to_s ).not_to eq( board.to_s )
-  end
+  end unless @disabled
+
   
   it "should be able to detect if the princess can reach the the knight with no pieces in between" do
     board = Board.new
@@ -262,7 +285,8 @@ describe Board do
 
     expect( board.princess_can_reach_knight? ).to eq( true )
     
-  end
+  end unless @disabled
+
 
   it "should be able to detect that the princess can't reach the knight" do
     board = Board.new
@@ -276,7 +300,8 @@ describe Board do
     expect( board.place_piece( red_piece, red_piece.positions.first, 3, 0 ) ).to eq( true )
 
     expect( board.princess_can_reach_knight? ).to eq( false )
-  end
+  end unless @disabled
+  
 
   it "should bo able to handle level changes and detect if the princess can reach the knight" do
     board = Board.new
@@ -293,7 +318,8 @@ describe Board do
     expect( board.place_piece( knight, knight.positions.first, 4, 1 ) ).to eq( true )
     
     expect( board.princess_can_reach_knight? ).to eq( true )
-  end
+  end unless @disabled
+
   
   it "should be able to detect that the princess can reach the knight if there are stairs in between" do
     board = Board.new
@@ -308,7 +334,8 @@ describe Board do
     expect( board.place_piece( blue_piece, Position.new( blue_piece, HORIZONTAL, [1, '\\'] ), 3, 0  ) ).to eq( true )
     expect( board.place_piece( knight, knight.positions.first, 5,0 ) ).to eq( true )
     expect( board.princess_can_reach_knight? ).to eq( true )
-  end
+  end unless @disabled
+
   
 
   it "should be able to find all the locations between the princess and the knight with no pieces in between" do
@@ -322,7 +349,8 @@ describe Board do
 
     correct_solution = [[1,0],[2,0],[3,0],[4,0]]
     expect( board.locations_between_princess_and_knight ).to eq( correct_solution )
-  end
+  end unless @disabled
+
   
   it "should be able to detect if the princess can reach the knight with pieces in between" do
     board = Board.new
@@ -337,7 +365,8 @@ describe Board do
 
     correct_solution = [[1,0],[2,0],[3,2]]
     expect( board.locations_between_princess_and_knight ).to eq( correct_solution )
-  end
+  end unless @disabled
+
 
 
   it "should be able to detect a complex layout where the princess can reach the knight" do
@@ -356,6 +385,20 @@ describe Board do
 
     expect( board.place_piece( knight, knight.positions.first, 4,2 ) ).to eq( true )
     expect( board.princess_can_reach_knight? ).to eq( true )
-  end
+  end unless @disabled
+
+
+  it "should not be able to place something very very tall on to the board" do
+    board = Board.new
+    red_piece_one = RedPiece.new
+    red_piece_two = RedPiece.new
+    red_piece_three = RedPiece.new
+    
+    expect( board.place_piece( red_piece_one, Position.new( red_piece_one, VERTICAL, red_piece_one.positions.first ), 0, 0  ) ).to eq( true )
+    expect( board.place_piece( red_piece_two, Position.new( red_piece_two, VERTICAL, red_piece_two.positions.first ), 0, 2  ) ).to eq( true )
+    expect( board.place_piece( red_piece_three, Position.new( red_piece_three, VERTICAL, red_piece_three.positions.first ), 0, 4  ) ).to eq( false )
+  end unless @disabled
+
   
-end
+  end 
+
