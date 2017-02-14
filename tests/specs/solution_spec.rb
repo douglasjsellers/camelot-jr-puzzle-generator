a2d6c759f71327cee90bbdfa8c7e30fe2c97105e
Douglas Sellers <douglas.sellers@gmail.com>
2017-02-13 23:11:20 -0800

    classic puzzle 25 is verified

--- tests/specs/solution_spec.rb
+++ tests/specs/solution_spec.rb
@@ -30,6 +30,33 @@ describe Solution do
     expect( final_board ).not_to eq( nil )
     
   end unless @disabled
+
+  it "should be able to solve classic puzzle 25" do
+    board = Board.new
+
+    orange_piece_one = OrangePiece.new
+    orange_piece_two = OrangePiece.new
+    red_piece = RedPiece.new
+    red_piece_two = RedPiece.new
+    
+    knight = Knight.new
+    princess = Princess.new
+
+    expect( board.place_piece( red_piece, red_piece.positions.first, 1, 0 ) ).to eq( true )
+    expect( board.place_piece( red_piece_two, red_piece_two.positions.first, 4, 0 ) ).to eq( true )    
+    
+    expect( board.place_piece( orange_piece_two, orange_piece_two.positions.first, 3, 0 ) ).to eq( true )
+    expect( board.place_piece( orange_piece_one, orange_piece_one.positions.first, 4, 2 ) ).to eq( true )
+    
+    expect( board.place_piece( princess, princess.positions.first, 1, 2 ) ).to eq( true )
+    expect( board.place_piece( knight, knight.positions.first, 4, 3 ) ).to eq( true )
+
+    solution = Solution.new( board, [PurplePiece.new, BluePiece.new] )
+    
+    expect( solution.has_solution? ).to eq( true )
+    final_board = solution.final_position_board
+    expect( final_board ).not_to eq( nil )
+  end unless @disabled
   
   it "should be able to solve classic puzzle 26" do
     board = Board.new
