3e98f49624645cf040e0b1060ffb79e8a50224f5
Douglas Sellers <douglas.sellers@gmail.com>
2017-02-13 23:34:52 -0800

    classic puzzle 13 is verified

--- tests/specs/solution_spec.rb
+++ tests/specs/solution_spec.rb
@@ -3,6 +3,33 @@ require_relative '../spec_helper'
 describe Solution do
   @disabled = true
 
+  it "should be able to solve classic puzzle 13" do
+    board = Board.new
+
+    orange_piece_one = OrangePiece.new
+    orange_piece_two = OrangePiece.new
+    red_piece = RedPiece.new
+    red_piece_two = RedPiece.new
+    
+    knight = Knight.new
+    princess = Princess.new
+
+    expect( board.place_piece( red_piece, red_piece.positions.first, 2, 0 ) ).to eq( true )
+    expect( board.place_piece( red_piece_two, red_piece_two.positions.first, 4, 0 ) ).to eq( true )    
+    
+    expect( board.place_piece( orange_piece_two, orange_piece_two.positions.first, 0, 0 ) ).to eq( true )
+    expect( board.place_piece( orange_piece_one, orange_piece_one.positions.first, 4, 2 ) ).to eq( true )
+    
+    expect( board.place_piece( princess, princess.positions.first, 0, 1 ) ).to eq( true )
+    expect( board.place_piece( knight, knight.positions.first, 4, 3 ) ).to eq( true )
+
+    solution = Solution.new( board, [BluePiece.new, BluePiece.new, PurplePiece.new] )
+    
+    expect( solution.has_solution? ).to eq( true )
+    final_board = solution.final_position_board
+    expect( final_board ).not_to eq( nil )
+  end unless @disabled
+  
   it "should be able to solve classic puzzle 14" do
     board = Board.new
 
