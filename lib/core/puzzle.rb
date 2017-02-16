fada4e6cc01c0f0aed6c71421233a24c9da59311
Douglas Sellers <douglas.sellers@gmail.com>
2017-02-15 18:19:37 -0800

    I think that the puzzle generator is working

--- lib/core/puzzle.rb
+++ lib/core/puzzle.rb
@@ -1,3 +1,35 @@
 class Puzzle
+  attr_reader :valid_boards
+  def initialize( puzzle_pieces, solving_pieces )
+    @puzzle_pieces = puzzle_pieces + [Knight.new, Princess.new]
+    @solving_pieces = solving_pieces
+    @valid_boards = []
+
+    compute_valid_boards
+  end
+
+  private
+  def add_next_piece_to_board( board, used_pieces, unused_pieces )
+    if( unused_pieces.empty? )
+      solution = Solution.new( board, @solving_pieces )
+      @valid_boards << board if solution.has_solution?
+    else
+      piece = unused_pieces.first
+      valid_positions = board.placeable_positions
+      valid_positions.each do |position|
+        position_board = board.clone
+        x, y = *position
+        position_board.place_piece( piece, piece.positions.first, x, y )
+        add_next_piece_to_board( position_board, used_pieces + [piece], unused_pieces - [piece] )
+      end
+    end
+    
+  end
+  
+  def compute_valid_boards
+    add_next_piece_to_board( Board.new, [], @puzzle_pieces )
+  end
+  
+  
 end
 
--- tests/specs/puzzle_spec.rb
+++ tests/specs/puzzle_spec.rb
@@ -1,4 +1,10 @@
 require_relative '../spec_helper'
 
 describe Puzzle do
+  it "should be able to run the constructor" do
+    puzzle = Puzzle.new( [OrangePiece.new],[ BluePiece.new, BluePiece.new] )
+
+    expect( puzzle.valid_boards.size).to eq( 2 )
+  end
+  
 end
