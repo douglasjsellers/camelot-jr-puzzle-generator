1c1fc2a457087ffbf15c383e0addef77ec17cd78
Douglas Sellers <douglas.sellers@gmail.com>
2017-02-18 15:21:41 -0800

    Added in the test for rendering knight and princess

--- tests/specs/rendered_board_spec.rb
+++ tests/specs/rendered_board_spec.rb
@@ -1,15 +1,30 @@
 require_relative '../spec_helper'
 
 describe RenderedBoard do
-  it "should be able to run the initializer" do
-    expect( RenderedBoard.new( Board.new ) ).not_to eq( nil )
-  end
+  @disabled = false
 
+  it "should be able to generate a rendered file with a knight and a princess on it" do
+    board = Board.new
+    knight = Knight.new
+    princess = Princess.new
+
+    expect( board.place_piece( knight, knight.positions.first, 4, 0 ) ).to eq( true )
+    expect( board.place_piece( princess, princess.positions.first, 0, 0 ) ).to eq( true )
+
+    rendered_board = Board.new( board )
+    file = rendered_board.rendered_file( 'result.png' ) 
+    expect( file ).not_to eq( nil )
+    
+  end
+  
   it "should be able to generate a simple rendered file with a blank board" do
     rendered_board = RenderedBoard.new( Board.new )
     file = rendered_board.rendered_file( 'result.png' ) 
     expect( file ).not_to eq( nil )
-  end
+  end unless @disabled
   
+  it "should be able to run the initializer" do
+    expect( RenderedBoard.new( Board.new ) ).not_to eq( nil )
+  end unless @disabled
   
 end
