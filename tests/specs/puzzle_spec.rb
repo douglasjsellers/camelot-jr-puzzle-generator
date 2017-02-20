7b043762106814f2b8849a15fa831bb24d0656fa
Douglas Sellers <douglas.sellers@gmail.com>
2017-02-20 11:16:24 -0800

    Removed the run all boards spec so that the tests can all be run

--- tests/specs/puzzle_spec.rb
+++ tests/specs/puzzle_spec.rb
@@ -3,13 +3,9 @@ require_relative '../spec_helper'
 describe Puzzle do
   it "should be able to run the constructor" do
     puzzle = Puzzle.new( [OrangePiece.new],[ BluePiece.new, BluePiece.new] )
-
     expect( puzzle.valid_boards.size).to eq( 2 )
   end
 
-  it "should be able to solve all boards" do
-    Puzzle.find_all_boards
-  end
   
   
 end
